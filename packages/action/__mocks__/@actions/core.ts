cbdf3ff5a12e687cfbf1e77bc352c522cb21ac54
Yukai Chou <muzimuzhi@gmail.com>
2026-03-15 12:02:34 +0800

    chore: make fmt-check happy

--- packages/action/__mocks__/@actions/core.ts
+++ packages/action/__mocks__/@actions/core.ts
@@ -2,23 +2,29 @@ import { vi } from 'vitest';
 
 import type { InputOptions } from '@actions/core';
 
-export const getInput = vi.fn((name: string, options?: InputOptions): string => {
-  const val =
-    globalThis.process.env[`INPUT_${name.replace(/ /gu, '_').toUpperCase()}`]
-    ?? '';
-  if (options?.required === true && !val) {
-    throw new Error(`Input required and not supplied: ${name}`);
-  }
-  return options?.trimWhitespace === false ? val : val.trim();
-});
+export const getInput = vi.fn(
+  (name: string, options?: InputOptions): string => {
+    const val =
+      globalThis.process.env[`INPUT_${name.replace(/ /gu, '_').toUpperCase()}`]
+        ?? '';
+    if (options?.required === true && !val) {
+      throw new Error(`Input required and not supplied: ${name}`);
+    }
+    return options?.trimWhitespace === false ? val : val.trim();
+  },
+);
 
 export const getBooleanInput = vi.fn(
   (name: string, options?: InputOptions): boolean => {
     const trueValue = ['true', 'True', 'TRUE'];
     const falseValue = ['false', 'False', 'FALSE'];
     const val = getInput(name, options);
-    if (trueValue.includes(val)) return true;
-    if (falseValue.includes(val)) return false;
+    if (trueValue.includes(val)) {
+      return true;
+    }
+    if (falseValue.includes(val)) {
+      return false;
+    }
     throw new TypeError(
       `Input does not meet YAML 1.2 "Core Schema" specification: ${name}`,
     );
