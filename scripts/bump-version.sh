8d811eeef903ea60a8c127ff9ad9ee860bbf1676
Yukai Chou <muzimuzhi@gmail.com>
2026-03-16 22:28:15 +0800

    chore: allow tweaking release note

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -17,12 +17,21 @@ case "${npm_lifecycle_event}" in
     fi
     ;;
 
-  tag)
+  tag:prep)
+    # Generate the changelog then stop for final tweaking.
     git ls-files -z dist |
       xargs -0 git update-index --assume-unchanged --
+    trap 'git ls-files -z dist | xargs -0 git update-index --no-assume-unchanged --' EXIT
 
-    npm run --silent changelog -- --tag "${version}" |
-      git tag "${version}" --cleanup=whitespace -F -
+    npm run --silent changelog -- --tag "${version}" > RELEASE_NOTE.md
+    echo "Release note for ${version} has been generated in RELEASE_NOTE.md"
+    echo -e "\n"
+    cat RELEASE_NOTE.md
+    echo
+    ;;
+
+  tag)
+    git tag "${version}" --cleanup=whitespace -F RELEASE_NOTE.md
     git tag -f "${version%%.*}" -m "${version}"
 
     git --no-pager show --color --no-patch "${version}" |
